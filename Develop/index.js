7a3016b3c7758378c66783bdfdb5c6d8f19fd473
emulcahey <e.yehaclum@gmail.com>
2024-05-28 01:16:40 -0500

    initial push

--- Develop/index.js
+++ Develop/index.js
@@ -11,3 +11,15 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+// title of my project, Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questionsgit
